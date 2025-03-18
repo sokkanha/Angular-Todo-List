5617bb8895ee2ea29e11f038bcc09e2fef4142d0
sokkanha <121841538+sokkanha@users.noreply.github.com>
2025-03-18 16:03:13 +0700

    Implement on filter data and custom darkMode UI

--- src/app/pages/todo/todo-lists/todo-lists.component.ts
+++ src/app/pages/todo/todo-lists/todo-lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TodoDetailsComponent } from '../todo-details/todo-details.component';
 import { TodoService, Todo } from '../../../services/todo.service';
@@ -11,8 +11,12 @@ import { TodoService, Todo } from '../../../services/todo.service';
 })
 export class TodoListsComponent implements OnInit {
   public listTodo: Todo[] = []; 
+  public dataItems: any =[];
   public isCheck:boolean = false;
-  
+  public taskName: string = '';
+  public taskFilter: string = '';
+  public isDark: boolean = false;
+
   constructor(
     private todoService:TodoService,
     private dialog:MatDialog
@@ -21,11 +25,12 @@ export class TodoListsComponent implements OnInit {
   ngOnInit(): void {
     this.getTodoLists(); 
   }
-
+  
   private getTodoLists() {
     this.todoService.list().subscribe(res => {
       if(!res) return;
       this.listTodo = res;
+      this.filterData();
     })
   }
 
@@ -51,5 +56,30 @@ export class TodoListsComponent implements OnInit {
   
   private updatedStatus(id:number, data:any) {
     this.todoService.update(id,data).subscribe();
+  }
+
+  public filterData() {
+    const filterData = this.taskName.trim() === '' 
+    ? this.listTodo 
+    : this.listTodo.filter(task =>
+      task.title.toLowerCase().includes(this.taskName.toLowerCase())
+    );
+
+    if (this.taskFilter === 'Complete') {
+      this.dataItems = filterData.filter(task => task.completed);
+    } else if (this.taskFilter === 'InComplete') {
+      this.dataItems = filterData.filter(task => !task.completed);
+    } else {
+      this.dataItems = filterData; // 'ALL' selected, no filtering by status
+    }
+  }
+
+  public toggleDarkMode() {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      document.body.classList.add('dark-mode');  // Add dark mode class to body
+    } else {
+      document.body.classList.remove('dark-mode');  // Remove dark mode class from body
+    }
   }
 }
--- src/app/pages/todo/todo-routing.module.ts
+++ src/app/pages/todo/todo-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListsComponent } from './todo-lists/todo-lists.component';
-import { TodoDetailsComponent } from './todo-details/todo-details.component';
 
 const routes: Routes = [
-  {path: '', component: TodoListsComponent},
-  {path: 'add', component: TodoDetailsComponent},
-  {path: 'edit/:id', component: TodoDetailsComponent},
-
+  {path: '', component: TodoListsComponent}
 ];
 
 @NgModule({
